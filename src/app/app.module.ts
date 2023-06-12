32555e79bdadb49e00fe3b0053fb9d348b11ce7d
Kenan Taşdemir <kenant103@gmail.com>
2023-06-12 23:17:55 +0300

    reactive forms studies

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,22 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CardformComponent } from './cardform/cardform.component';
+import { InputComponent } from './input/input.component';
+import { ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CardformComponent,
+    InputComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    ReactiveFormsModule,
+
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/cardform/cardform.component.ts
@@ -0,0 +1,47 @@
+import { Component,OnInit } from '@angular/core';
+import {FormGroup,FormControl,Validators} from "@angular/forms"
+import { DateFormControl } from '../dateformcontrol';
+
+@Component({
+  selector: 'app-cardform',
+  templateUrl: './cardform.component.html',
+  styleUrls: ['./cardform.component.css']
+})
+export class CardformComponent implements OnInit {
+  cardForm = new FormGroup({
+    name:new FormControl(null,[
+      Validators.required,
+      Validators.minLength(3)
+    ]),
+    cardNumber:new FormControl(null,[
+      Validators.required,
+      Validators.minLength(16),
+      Validators.maxLength(16),
+    ]),
+    expiration:new DateFormControl(null,[
+      Validators.required,
+      Validators.pattern(/^(0[1-9]|1[0-2])\/\d{2}$/)
+    ]),
+    securityCode:new FormControl(null,[
+      Validators.required,
+      Validators.minLength(3),
+      Validators.maxLength(3)
+    ]),
+  })
+
+
+  constructor() {  }
+
+  ngOnInit(): void {
+  }
+  onSubmit()
+  {
+    console.log("Can");
+  }
+
+  resetForm()
+  {
+    this.cardForm.reset();
+  }
+
+}
